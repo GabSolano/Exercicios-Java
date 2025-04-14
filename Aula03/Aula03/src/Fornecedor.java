ff46c71ebb9ea3c39e1fdd86948e2af91a3402e6
Gabriel Solano <81971607+GabSolano@users.noreply.github.com>
2025-04-14 20:19:45 -0300

    Delete Aula03 directory

--- Aula03/Aula03/src/Fornecedor.java
+++
@@ -1,8 +0,0 @@
-public class Fornecedor {
-
-    String nome;
-    String telefone;
-    String cnpj;
-}
-
-
--- Aula03/Aula03/src/Produtos.java
+++
@@ -1,8 +0,0 @@
-public class Produtos {
-    //atributos
-    String nome;
-    int codigo;
-    float preco;
-    boolean disponivel;
-    Fornecedor fornecedor;
-}
